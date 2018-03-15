c01242ea8f4d29303e37672cc002df4979c1aaf3
Andrew Zoka <mugendiz@gmail.com>
2018-03-15 19:47:36 -0400

    added events.js

---
+++ public/assets/js/events.js
@@ -0,0 +1,40 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: ""
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+// Initial Values
+var initialBid = 0;
+var initialBidder = "No one :-(";
+var highPrice = initialBid;
+var highBidder = initialBidder;
+
+// --------------------------------------------------------------
+
+// At the initial load and subsequent value changes, get a snapshot of the stored data.
+// This function allows you to update your page in real-time when the firebase database changes.
+database.ref().on("value", function(snapshot) {
+
+  // If Firebase has a highPrice and highBidder stored (first case)
+  if (snapshot.child("highBidder").exists() && snapshot.child("highPrice").exists()) {
+    // Set the variables for highBidder/highPrice equal to the stored values.
+    highBidder = snapshot.val().highBidder;
+    highPrice = parseInt(snapshot.val().highPrice);
+
+    // Change the text inside the HTML element to reflect the initial value
+    $("#highest-bidder").text(snapshot.val().highBidder);
+    $("#highest-price").text("$" + snapshot.val().highPrice);
+
+    // Print the data to the console.
+    console.log(snapshot.val().highBidder);
+    console.log(snapshot.val().highPrice);
+  }
