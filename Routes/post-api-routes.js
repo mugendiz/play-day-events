b9a1a586a5df081ff44bac054b0b66d7da58be36
Deniz Aksoylu <aksoyludeniz@outlook.com>
2018-03-24 00:33:10 -0400

    bug in java regarding ajax call

--- Routes/post-api-routes.js
+++ Routes/post-api-routes.js
@@ -2,75 +2,97 @@
 // =============================================================
 
 // Requiring our models
-var db = require("../models");
+var eventArray = require("../models/format")
+// var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function (app) {
+module.exports = function(app) {
 
-    // GET route for getting all of the events
-    app.get("/api/events", function (req, res) {
-        var query = {};
-        if (req.query.id) {
-            query.Event_Name = req.query.id;
-        }
-        // Here we add an "include" property to our options in our findAll query
-        // We set the value to an array of the models we want to include in a left outer join
-        // In this case, just db.Name
-        db.events_DB.findAll({
-            where: query,
-            include: [db.events]
-        }).then(function (dbevent) {
-            res.json(dbevent);
-        });
+  app.get("/api/form",function(req, res) {
+    res.json(eventArray);
+  });
+
+  app.post("/api/form", function(req, res) {
+      eventArray.push(req.body);
+      res.json(true);
     });
 
-    // Get rotue for retrieving a single events
-    app.get("/api/events/:id", function (req, res) {
-        // Here we add an "include" property to our options in our findOne query
-        // We set the value to an array of the models we want to include in a left outer join
-        // In this case, just db.Name
-        db.events.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [db.events]
-        }).then(function (events_DB) {
-            res.json(events_DB);
-        });
-    });
 
-    // POST route for saving a new post
-    app.post("/api/events", function (req, res) {
-        db.events.create(req.body).then(function (events_DB) {
-            res.json(events_DB);
-        });
-    });
 
-    // DELETE route for deleting posts
-    app.delete("/api/events/:id", function (req, res) {
-        db.events.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (events_DB) {
-            res.json(events_DB);
-        });
-    });
 
-    // PUT route for updating posts
-    app.put("/api/events", function (req, res) {
-        db.events.update(
-            req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
-            }).then(function (events_DB) {
-                res.json(events_DB);
-            });
-    });
+  // app.post("/api/clear", function() {
+  //   // Empty out the arrays of data
+  //   eventArray = [];
+  //
+  //   console.log(eventArray);
+  // });
 };
+
+    // // GET route for getting all of the events
+    // app.get("/api/events", function (req, res) {
+    //     var query = {};
+    //     if (req.query.id) {
+    //         query.Event_Name = req.query.id;
+    //     }
+    //     // Here we add an "include" property to our options in our findAll query
+    //     // We set the value to an array of the models we want to include in a left outer join
+    //     // In this case, just db.Name
+    //     db.events_DB.findAll({
+    //         where: query,
+    //         include: [db.events]
+    //     }).then(function (dbevent) {
+    //         res.json(dbevent);
+    //     });
+    // });
+    //
+    // // Get rotue for retrieving a single events
+    // app.get("/api/events/:id", function (req, res) {
+    //     // Here we add an "include" property to our options in our findOne query
+    //     // We set the value to an array of the models we want to include in a left outer join
+    //     // In this case, just db.Name
+    //     db.events.findOne({
+    //         where: {
+    //             id: req.params.id
+    //         },
+    //         include: [db.events]
+    //     }).then(function (events_DB) {
+    //         res.json(events_DB);
+    //     });
+    // });
+    //
+    // // POST route for saving a new post
+    // app.post("/api/events", function (req, res) {
+    //     db.events.create(req.body).then(function (events_DB) {
+    //         res.json(events_DB);
+    //     });
+    // });
+    //
+    // // DELETE route for deleting posts
+    // app.delete("/api/events/:id", function (req, res) {
+    //     db.events.destroy({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     }).then(function (events_DB) {
+    //         res.json(events_DB);
+    //     });
+    // });
+    //
+    // // PUT route for updating posts
+    // app.put("/api/events", function (req, res) {
+    //     db.events.update(
+    //         req.body,
+    //         {
+    //             where: {
+    //                 id: req.body.id
+    //             }
+    //         }).then(function (events_DB) {
+    //             res.json(events_DB);
+    //         });
+    // });
+
+
 
 
 // Dependencies
