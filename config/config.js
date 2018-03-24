9d9cf98f7164a6ec22a869e41dc5c4175da056cf
Amber <arunge33@gmail.com>
2018-03-23 21:53:09 -0400

    api formatting changes

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "oliver",
     "database": "events_DB",
     "host": "127.0.0.1",
     "dialect": "mysql"
