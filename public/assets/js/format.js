73271085df0230d180b5f44278e910ea0115a04b
Andrew Zoka <mugendiz@gmail.com>
2018-03-25 22:34:09 -0400

    final changes before deployment 1, March 25 2018

--- public/assets/js/format.js
+++ public/assets/js/format.js
@@ -1,64 +1,67 @@
+function runEventQuery() {
+
+  // Here we get the location of the root page.
+  // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+  var currentURL = window.location.origin;
+
+  // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+  $.ajax({
+      url: currentURL + "/api/form",
+      method: "GET"
+    })
+    .done(function(eventArray) {
+
+      // Here we are logging the URL so we have access to it for troubleshooting
+      console.log("------------------------------------");
+      console.log("URL: " + currentURL + "/api/form");
+      console.log("------------------------------------");
+
+      // Here we then log the NYTData to console, where it will show up as an object.
+      console.log(eventArray);
+      console.log("------------------------------------");
+
+      // Loop through and display each of the customers
+      for (var i = 0; i < eventArray.length; i++) {
+
+        // Create the HTML Well (Section) and Add the table content for each reserved table
+        var eventSection = $("<div>");
+        eventSection.addClass("formID");
+        eventSection.addClass("eventLoc");
+        eventSection.addClass("eventDate");
+        eventSection.addClass("eventTime");
+        eventSection.attr("formID", "eventLoc", "eventDate"
+          "eventTime" + i + 1);
+        $("#eventSection").append(eventSection);
+
+        var eventNumber = i + 1;
+
+        // Then display the remaining fields in the HTML (Section Name, Date, URL)
+        $("#eventWell-" + i + 1).append("<h2><span class='form-group'>" + eventName + "</span> | " + eventArray[i].customerID + "</h2>");
+      }
+    });
+}
 
 
+function clearEvent() {
 
-  function runEventQuery() {
-
-      // Here we get the location of the root page.
-      // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
-      var currentURL = window.location.origin;
-
-      // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
-      $.ajax({ url: currentURL + "/api/form", method: "GET" })
-      .done(function(eventArray) {
-
-        // Here we are logging the URL so we have access to it for troubleshooting
-        console.log("------------------------------------");
-        console.log("URL: " + currentURL + "/api/form");
-        console.log("------------------------------------");
-
-        // Here we then log the NYTData to console, where it will show up as an object.
-        console.log(eventArray);
-        console.log("------------------------------------");
-
-        // Loop through and display each of the customers
-        for (var i = 0; i < eventArray.length; i++) {
-
-          // Create the HTML Well (Section) and Add the table content for each reserved table
-          var eventSection = $("<div>");
-          eventSection.addClass("formID");
-          eventSection.addClass("eventLoc");
-          eventSection.addClass("eventDate");
-          eventSection.addClass("eventTime");
-          eventSection.attr("formID", "eventLoc", "eventDate" "eventTime" + i + 1);
-          $("#eventSection").append(eventSection);
-
-          var eventNumber = i + 1;
-
-
-          // Then display the remaining fields in the HTML (Section Name, Date, URL)
-          $("#eventWell-" + i + 1).append("<h2><span class='form-group'>" + eventName + "</span> | " + eventArray[i].customerID + "</h2>");
-        }
-      });
-    }
-
-
-  function clearEvent() {
-
-    var currentURL = window.location.origin;
-    $.ajax({ url: currentURL + "/api/clear", method: "POST" });
-
-  }
-
-  $("#clear").on("click", function() {
-    alert("Clearing...");
-    clearEvent();
-
-    // Refresh the page after data is cleared
-    location.reload();
-
+  var currentURL = window.location.origin;
+  $.ajax({
+    url: currentURL + "/api/clear",
+    method: "POST"
   });
 
+}
 
-  // Run Queries!
-  // ==========================================
-  runEventQuery();
+$("#clear").on("click", function() {
+  alert("Clearing...");
+  clearEvent();
+
+  // Refresh the page after data is cleared
+  location.reload();
+
+});
+
+
+// Run Queries!
+// ==========================================
+runEventQuery();
--- public/assets/js/parents.js
+++ public/assets/js/parents.js
@@ -1,11 +1,11 @@
 function apiCallss() {
   var see = $('#see');
   // Here we construct our URL
-  var queryURL = "https://www.eventbriteapi.com/v3/events/search/?categories=115&subcategories=15006&location.address=new%20york&token=S6QQEW7L4B5Z4NSM6VOC";
+  var queryURL = 'https://www.eventbriteapi.com/v3/events/search/?location.address=maryland%2Cvirginia&categories=115&subcategories=15006&token=S6QQEW7L4B5Z4NSM6VOC';
 
   $.ajax({
     url: queryURL,
-    method: "GET"
+    method: 'GET'
   }).done(function(response) {
     console.log(response);
     for (var i = 0; i < response.events.length; i++) {
@@ -15,15 +15,14 @@ function apiCallss() {
           html += response.events[i].name.text
           html += "</div>"
           html += "<div class ='description'>"
-          html += response.events[i].name.description
+          // html += "<p> " + response.events[i].description.html + "</p>"
+          html += "Click image for more information and to register"
+          html += "<a href="  + response.events[i].url + ">"
           html += "</div>"
           html += "<div class = 'image_container'>"
           html += "<img src = "+response.events[i].logo.original.url+">"
           html += "</div>"
           html += "<div class = 'image_url'>"
-          html += "<href = "+response.events[i].url+">"
-
-          html += "</div>"
           html += "</div>"
 
 
@@ -33,5 +32,42 @@ function apiCallss() {
   });
 }
 
-
 window.onload = apiCallss;
+
+// API Call for search
+
+function searchApiCall() {
+  var show = $('#show');
+  // Here we construct our URL
+  var queryURL2 = 'https://www.eventbriteapi.com/v3/events/search/?location.address=district+of+columbia&categories=115&subcategories=2002%2C4003%2C4004%2C4005%2C5003%2C3016&token=S6QQEW7L4B5Z4NSM6VOC';
+
+  var searchQuery = $('#search').val().trim();
+
+  $.ajax({
+    url: queryURL2,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response);
+    for (var i = 0; i < response.events.length; i++) {
+      console.log(response.events[i].name.text);
+      var html = "<div class ='card_container'>"
+      html += "<div class ='title'>"
+      html += response.events[i].name.text
+      html += "</div>"
+      html += "<div class ='description'>"
+      // html += response.events[i].description.html
+      html += "Click image for more information and to register"
+      html += "<a href=" + response.events[i].url + ">"
+      html += "<div class = 'image_container'>"
+      html += "<img src = " + response.events[i].logo.original.url + ">"
+      html += "<div class = 'image_url'>"
+      html += "<href = " + response.events[i].url + ">"
+      html += "</div>"
+      html += "</div>"
+
+      show.append(html)
+    }
+  });
+}
+
+$('#searchBtn').click(searchApiCall());
